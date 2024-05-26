83642b8b191efd0761b154475c43287e28d6600c
Yracerlyn <yessicerlyn@gmail.com>
2024-05-26 13:40:13 +0200

    initialisation Api et connexion à mongo express JS

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+const multer = require("multer");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const CONNECTION_STRING = "mongodb+srv://yessicerlyn:yWzjvBu8fflBHgsC@projetweb.xgwxztb.mongodb.net/?retryWrites=true&w=majority&appName=projetweb";
+const DATABASENAME = "projetWeb";
+let database;
+
+app.listen(5038, () => {
+    console.log("Serveur démarré sur le port 5038");
+    MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(client => {
+            database = client.db(DATABASENAME);
+            console.log("Connexion à ta grand mère mongo réussi !");
+        })
+        .catch(error => {
+            console.error("Erreur de connexion à MongoDB :", error);
+        });
+});
